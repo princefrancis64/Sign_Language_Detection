372e51a71ef0f0d88f32b27923c0dc84565a2f13
princefrancis64 <prince.francis64@gmail.com>
2024-04-01 13:29:35 +0530

    requirements added'

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='signLanguage',
+    version="0.0.1",
+    author="Prince Francis",
+    author_email="prince.francis64@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+    )
